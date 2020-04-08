bb42efaedd5a840ca061de1bde6f2fdb9f934909
Marcus Breese <marcus@breese.com>
2020-04-08 04:58:38 -0400

    submodule update

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,14 @@ if [ ! -e venv ]; then
     ./install.sh
 fi
 
+if [ -e site ]; then
+    rm -rf site
+fi
+
 git submodule update --remote
 git commit -am 'submodule update'
 
 venv/bin/mkdocs build
-
 cd site
 cp -r ../modules/* .
 rm -rf */.git
@@ -19,4 +22,4 @@ git remote add origin https://github.com/compgen-io/compgen-io-docs.git
 git checkout -b gh-pages
 git push --force origin gh-pages
 cd ..
-rm -rf site
+#rm -rf site
